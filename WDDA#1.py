77276a3d55a8b776b433452cd9f784f321ab2f2e
Common-Man-2077 <common.man2077@gmail.com>
2020-09-16 21:53:29 +1000

    Create WDDA#1.py

---
+++ WDDA#1.py
@@ -0,0 +1,34 @@
+# Import JSON package and API requests Library
+import json
+import requests
+import os
+import time
+# Locate the file directory of this python file
+# change the working directory to this location
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+# This is to print the total operation time
+start_time = time.time()
+
+i = 1
+data = []
+while i < 244:
+    link = "https://theyvoteforyou.org.au/api/v1/policies/" + \
+        str(i) + ".json?key=QNhXYGZ%2FWfCivHEXRVVj"
+    response = requests.get(
+        link)
+    item_dict = response.json()
+    data.append({
+        'name': item_dict['name'],
+        'number of division to pass': len(item_dict['policy_divisions']),
+    })
+    i = i + 1
+
+
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile)
+
+# # This is to print the total operation time
+print("--- %s seconds ---" % (time.time() - start_time))
