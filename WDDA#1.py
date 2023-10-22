44bab70f793595a5903142d90e3eb94b5f4670fc
Common-Man-2077 <common.man2077@gmail.com>
2023-10-22 12:02:31 +1100

    test

--- WDDA#1.py
+++ WDDA#1.py
@@ -3,7 +3,6 @@ import json
 import requests
 import os
 import time
-import numpy as np
 # Locate the file directory of this python file
 # change the working directory to this location
 abspath = os.path.abspath(__file__)
